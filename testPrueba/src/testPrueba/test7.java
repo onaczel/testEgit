2cd7075a8acc518f282f3e7372aa3be35c588c25
sergio <sergio@sergio-nb>
2015-03-09 11:19:52 -0300

    x

--- testPrueba/src/testPrueba/test7.java
+++ testPrueba/src/testPrueba/test7.java
@@ -5,10 +5,10 @@ public class test7 {
 	
 	//comentario de Sergio
 
-=======
+
 	//NEw comment super pro ola de mar
-<<<<<<< HEAD
+
 // hola, me sale error?
-=======
->>>>>>> branch 'master' of https://github.com/onaczel/testEgit.git
+
+
 }
